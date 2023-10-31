11d688c1040b15637bea774afc3da618e1a1da86
DIGONTA HALDER <digantahalder14000@gmail.com>
2023-10-31 09:46:36 +0530

    updated code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css"; //
 import "font-awesome/css/font-awesome.min.css";
-import "./index.css";
 import { AuthProvider } from "./store/auth-context.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
